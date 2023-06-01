7eaf6486cd5201acd1cb8fc45e9ba9f4a6aac2d2
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2023-06-02 04:19:04 +0530

    Create codeincpp.cpp

---
+++ Pre requisites/01) Arrays/1.2 Selection Sort/codeincpp.py
@@ -0,0 +1,36 @@
+
+
+#include <iostream>
+#include <vector>
+
+void selectionSort(std::vector<int>& array) {
+    int n = array.size();
+
+    for (int i = 0; i < n - 1; ++i) {
+        int min_index = i;
+
+        // Find the minimum element in the unsorted part of the array
+        for (int j = i + 1; j < n; ++j) {
+            if (array[j] < array[min_index]) {
+                min_index = j;
+            }
+        }
+
+        // Swap the minimum element with the first unsorted element
+        std::swap(array[i], array[min_index]);
+    }
+}
+
+int main() {
+    std::vector<int> myArray = {64, 25, 12, 22, 11};
+
+    selectionSort(myArray);
+
+    std::cout << "Sorted array: ";
+    for (int num : myArray) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
