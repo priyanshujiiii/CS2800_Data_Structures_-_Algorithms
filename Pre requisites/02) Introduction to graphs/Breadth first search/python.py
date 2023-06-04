b6fc5791472c540ad5778c801215a5c836a17040
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2023-06-04 16:50:32 +0530

    Update python.py

--- Pre requisites/02) Introduction to graphs/Breadth first search/python.py
+++ Pre requisites/02) Introduction to graphs/Breadth first search/python.py
@@ -1,1 +1,28 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=" ")
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+
+# Example usage:
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+start_node = 'A'
+bfs(graph, start_node)
 
