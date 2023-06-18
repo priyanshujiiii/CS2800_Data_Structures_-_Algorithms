90b772b43250ccbd40a1c34cf514c639142baacb
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2023-06-18 12:19:25 +0530

    commit

--- Level-1 Question/1 Array/1.01 Reverse the Array.py
+++ Level-1 Question/1 Array/1.01 Reverse the Array.py
@@ -1,9 +1,12 @@
 a = input()
-
+a=a.split(",")
+b1 = []
+for i in a:
+    b1 = b1 + [int(i)]
 def reverse(k):
-    l = ""
+    l = []
     for i in k:
-        l = i + l
+        l = [i] + l
     return l
 
-print(reverse(a))
+print(reverse(b1))
